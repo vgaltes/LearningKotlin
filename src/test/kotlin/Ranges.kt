c3c2e1502b110b0bcb3cd5f3aa48f61c896a06da
Vicenç Garcia <me@vgaltes.com>
2022-08-14 17:48:30 +0200

    Ranges

---
+++ src/test/kotlin/Ranges.kt
@@ -0,0 +1,43 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+data class RangeData(val minRange: Int, val maxRange: Int, val value: Int, val isInRange: Boolean)
+
+class Ranges : FunSpec({
+    context("two points can define a range") {
+        withData(
+            RangeData(1, 10, 5, true),
+            RangeData(1, 10, 15, false)
+        )
+        {
+            (minRange, maxRange, value, isInRange) -> {
+                val within = value in minRange..maxRange
+                within shouldBe isInRange
+            }
+        }
+    }
+
+    context("not within") {
+        withData(
+            RangeData(1, 10, 5, true),
+            RangeData(1, 10, 10, true),
+            RangeData(1, 10, 15, false)
+        )
+        {
+            (minRange, maxRange, value, isInRange) -> {
+                val notWithin = value !in minRange..maxRange
+                notWithin shouldBe !isInRange
+        }
+        }
+    }
+
+    test("you can use within with characters") {
+        ('c' in 'a'..'d') shouldBe true
+    }
+
+    test("you can use within with strings") {
+        ("he" in "ha".."hi") shouldBe true
+    }
+
+})
