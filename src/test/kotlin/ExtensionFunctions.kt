c7dec6a83b2b90ccb30df1a8e4ae0c37a0ddecb7
Vicenç Garcia <me@vgaltes.com>
2022-11-13 19:28:33 +0100

    complete extension functions

--- src/test/kotlin/ExtensionFunctions.kt
+++ src/test/kotlin/ExtensionFunctions.kt
@@ -1,7 +1,19 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
+class A {
+    fun member(): String = "hi from member"
+}
+
+val A.salutation: String
+    get() { return "Hi!"}
+
+class MyRepository<T> {
+    companion object { }
+}
+
 class ExtensionFunctions : StringSpec({
+
     "you can create a basic extension function" {
         fun String.repeated(): String = this + this
 
@@ -30,4 +42,52 @@ class ExtensionFunctions : StringSpec({
         str shouldBe "hi from member"
 
     }
+
+    "we can extend via a property as well" {
+        val a = A()
+        a.salutation shouldBe "Hi!"
+    }
+
+    "we can define extension methods inside a type and then it won't have global visibility" {
+        open class StringFormatter {
+            fun String.toUppercaseCustom():String = this.uppercase()
+        }
+
+        class SomeClass : StringFormatter() {
+            fun getUpperCase(input: String) = input.toUppercaseCustom()
+        }
+
+        val c = SomeClass()
+        c.getUpperCase("hello") shouldBe "HELLO"
+    }
+
+    "extension functions are statically resolved" {
+        open class User
+        class Customer : User()
+
+        fun User.presentMyself(): String = "Hi, I'm a user"
+        fun Customer.presentMyself(): String = "Hi, I'm a customer"
+
+        val user: User = User()
+        val customer: Customer = Customer()
+        val hiddenCustomer: User = Customer()
+
+        user.presentMyself() shouldBe "Hi, I'm a user"
+        customer.presentMyself() shouldBe "Hi, I'm a customer"
+        hiddenCustomer.presentMyself() shouldBe "Hi, I'm a user"
+    }
+
+    "can extend companion objects, nullable types, and generic types" {
+        fun MyRepository.Companion.find(): String = "found"
+        fun <T> MyRepository<T>?.findNullable(): String = "found"
+
+        MyRepository.find() shouldBe "found"
+
+        val r: MyRepository<String>? = null
+        r?.findNullable() shouldBe null
+
+        val r2: MyRepository<String>? = MyRepository<String>()
+        r2.findNullable() shouldBe "found"
+    }
 })
+
